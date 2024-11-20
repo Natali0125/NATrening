06d81b2eb5589c46dda420317bb11ff1fbc6da8d
Natali0125 <seminatalya2013@gmail.com>
2024-11-21 00:46:53 +0300

    Add files via upload
    Модуль_1(практ. задание переменные)

---
+++ modul_1_Peremennie.py
@@ -0,0 +1,5 @@
+completed_task_count = 12
+hour_count = 1.5
+name_course = 'Phyton'
+time_for_1task = hour_count/completed_task_count
+print('Курс:',name_course, ', всего задач:',  completed_task_count, ', затрачено часов:', hour_count,', среднее время выполнения:', time_for_1task, 'часа.' )
