982f6be710698ca23482dcc8171766e363dfe0a3
Natali0125 <seminatalya2013@gmail.com>
2024-12-18 17:22:11 +0300

    Add files via upload
    Модуль_4 Задание_1 (Домашняя работа по уроку "Модули и пакеты")

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fdiv
+from true_math import divide as thdiv
+
+result_1 = fdiv(69, 3)
+result_2 = fdiv(3,0)
+result_3 = thdiv(49, 7)
+result_4 = thdiv(15, 0)
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
+
